3b1bfb1d02fe5b72d7aa74436936d5b5497e8c67
shammai hamilton <shammaihamilton@gmail.com>
2024-05-31 18:38:42 +0300

    adding another case

--- src/main.py
+++ src/main.py
@@ -5,52 +5,62 @@ def main():
 
     name_processor = NameProcessor()
 
-    print("case 1 - extra character to remove, expect 1: ",
-          name_processor.count_unique_names("Deborah S", "Egli levi", "Deborah", "Egli levi", "Egli levi Deborah"))  # 1
-    print("case 2 - 2 first names, 1 last name, expect 1: ",
-          name_processor.count_unique_names("Deborah lea", "levi", "Deborah lea", "levi", "levi Deborah lea"))  # 1
-    print("case 3 - spelling check in first name, expect 1: ",
-          name_processor.count_unique_names("Deboah lea", "levi", "Deborah lea", "levi", "levi Debrah lea"))  # 1
-    print("case 4 - spelling check in middle and first and last names, expect 1: ",
-          name_processor.count_unique_names("Deborah le", "levi", "Deboah lea", "lei", "levi Deborah lea"))  # 1
-    print("case 5 - different first names, expect 2: ",
-          name_processor.count_unique_names("Michele", "Egli", "Deborah", "Egli", "Michele Egli"))  # 2
-    print("case 6 - different last names, expect 3: ",
-          name_processor.count_unique_names("Michele", "Egli", "Deborah", "Houri", "Michele Hamilton"))  # 3
-    print("case 7 - different nicknames to same person, expect 1: ",
-          name_processor.count_unique_names("Deborah", "Egli", "Debbie", "Egli", "Debbie Egli"))  # 1
-    print("case 8 - same name in different formats, expect 1: ",
-          name_processor.count_unique_names("Deborah", "Egli", "Deborah", "Egli", "Deborah Egli"))  # 1
-    print("case 9 - different nicknames to same person, expect 1: ",
-          name_processor.count_unique_names("Deborah", "Egli", "Debbie", "Egli", "Debbie Egli"))  # 1
-    print("case 10 - different spelling in last names, expect 1: ",
-          name_processor.count_unique_names("Deborah", "Egni", "Deborah", "Egli", "Deborah Egli"))  # 1
-    print("case 11 - last name then first name, expect 1: ",
-          name_processor.count_unique_names("Deborah S", "Egli", "Deborah", "Egli", "Egli Deborah"))  # 1
-    print("case 12 - different first names, expect 2: ",
-          name_processor.count_unique_names("Michele", "Egli", "Deborah", "Egli", "Michele Egli"))  # 2
-    print("case 13 - different nicknames, expect 2: ",
-          name_processor.count_unique_names("abigail", "Egni", "abby", "Egni", "al Egni"))  # 2
-    print("case 14 - different first names, expect 2: ",
-          name_processor.count_unique_names("Alexander ron", "Egni", "al", "Egni", "Deborah Egni"))  # 2
-    print("case 15 - different nicknames with middle name, expect 1: ",
-          name_processor.count_unique_names("Deborah", "Egni", "Debbie", "Egni", "Deborah you Egni"))  # 1
-    print("case 16 - different names, expect 2: ",
-          name_processor.count_unique_names("Deborah rer", "Egni", "Debbie", "Egni", "Michele ntr Egni"))  # 2
+    # print("case 1 - extra character to remove, expect 1: ",
+    #       name_processor.count_unique_names("Deborah S", "Egli levi", "Deborah", "Egli levi", "Egli levi Deborah"))  # 1
+    # print("case 2 - 2 first names, 1 last name, expect 1: ",
+    #       name_processor.count_unique_names("Deborah lea", "levi", "Deborah lea", "levi", "levi Deborah lea"))  # 1
+    # print("case 3 - spelling check in first name, expect 1: ",
+    #       name_processor.count_unique_names("Deboah lea", "levi", "Deborah lea", "levi", "levi Debrah lea"))  # 1
+    # print("case 4 - spelling check in middle and first and last names, expect 1: ",
+    #       name_processor.count_unique_names("Deborah le", "levi", "Deboah lea", "lei", "levi Deborah lea"))  # 1
+    # print("case 5 - different first names, expect 2: ",
+    #       name_processor.count_unique_names("Michele", "Egli", "Deborah", "Egli", "Michele Egli"))  # 2
+    # print("case 6 - different last names, expect 3: ",
+    #       name_processor.count_unique_names("Michele", "Egli", "Deborah", "Houri", "Michele Hamilton"))  # 3
+    # print("case 7 - different nicknames to same person, expect 1: ",
+    #       name_processor.count_unique_names("Deborah", "Egli", "Debbie", "Egli", "Debbie Egli"))  # 1
+    # print("case 8 - same name in different formats, expect 1: ",
+    #       name_processor.count_unique_names("Deborah", "Egli", "Deborah", "Egli", "Deborah Egli"))  # 1
+    # print("case 9 - different nicknames to same person, expect 1: ",
+    #       name_processor.count_unique_names("Deborah", "Egli", "Debbie", "Egli", "Debbie Egli"))  # 1
+    # print("case 10 - different spelling in last names, expect 1: ",
+    #       name_processor.count_unique_names("Deborah", "Egni", "Deborah", "Egli", "Deborah Egli"))  # 1
+    # print("case 11 - last name then first name, expect 1: ",
+    #       name_processor.count_unique_names("Deborah S", "Egli", "Deborah", "Egli", "Egli Deborah"))  # 1
+    # print("case 12 - different first names, expect 2: ",
+    #       name_processor.count_unique_names("Michele", "Egli", "Deborah", "Egli", "Michele Egli"))  # 2
+    # print("case 13 - different nicknames, expect 2: ",
+    #       name_processor.count_unique_names("abigail", "Egni", "abby", "Egni", "al Egni"))  # 2
+    # print("case 14 - different first names, expect 2: ",
+    #       name_processor.count_unique_names("Alexander ron", "Egni", "al", "Egni", "Deborah Egni"))  # 2
+    # print("case 15 - different nicknames with middle name, expect 1: ",
+    #       name_processor.count_unique_names("Deborah", "Egni", "Debbie", "Egni", "Deborah you Egni"))  # 1
+    # print("case 16 - different names, expect 2: ",
+    #       name_processor.count_unique_names("Deborah rer", "Egni", "Debbie", "Egni", "Michele ntr Egni"))  # 2
+    #
+    # print("case 17 - different family names, expect 3: ",
+    #       name_processor.count_unique_names("Deborah", "hamilton", "Deborah", "halton", "Deborah hamiln"))  # 3
+    # print("case 18 - simple check by remove space, expect 1: ",
+    #       name_processor.count_unique_names("Deborah", " Egli", "Deborah", " Egli", "Deborah Egli"))  # 1
+    # print("case 19 - first name with middle name and 2 last names, expect 1:",
+    #       name_processor.count_unique_names("Deborah lea", "Egli levi", "Deborah lea", "Egli levi",
+    #                                         "Egli levi Deborah lea"))  # 1
+    # print("case 20 - 1 first name, 2 last names, and a middle name in card, expect 1:",
+    #       name_processor.count_unique_names("Deborah", "Egli levi", "Deborah", "Egli levi",
+    #                                         "Egli levi Deborah lea"))  # 1
+    # print("case 21 - first name is first in card, expect 1: ",
+    #       name_processor.count_unique_names("Deborah", "Egli levi", "Deborah", "Egli levi", "Egli levi Deborah"))  # 1
 
-    # Original test cases
-    print("case 17 - different family names, expect 3: ",
-          name_processor.count_unique_names("Deborah", "hamilton", "Deborah", "halton", "Deborah hamiln"))  # 3
-    print("case 18 - simple check by remove space, expect 1: ",
-          name_processor.count_unique_names("Deborah", " Egli", "Deborah", " Egli", "Deborah Egli"))  # 1
-    print("case 19 - first name with middle name and 2 last names, expect 1:",
-          name_processor.count_unique_names("Deborah lea", "Egli levi", "Deborah lea", "Egli levi",
-                                            "Egli levi Deborah lea"))  # 1
-    print("case 20 - 1 first name, 2 last names, and a middle name in card, expect 1:",
-          name_processor.count_unique_names("Deborah", "Egli levi", "Deborah", "Egli levi",
-                                            "Egli levi Deborah lea"))  # 1
-    print("case 21 - first name is first in card, expect 1: ",
-          name_processor.count_unique_names("Deborah", "Egli levi", "Deborah", "Egli levi", "Egli levi Deborah"))  # 1
+# The code does not work correctly when we have typo and  nickname
+    print("case 22 - first name with typo  and nickname 1: ",
+          name_processor.count_unique_names("Deborah", "Egli", "Debbie", "Egli", "Egli  Deorah"))  # 1
+
+
+    print("case 23 - first name with typo  and nickname 1: ",
+          name_processor.count_unique_names("Deborah", "Egli", "Deborah", "Egli", "Egli  Dbbie"))  # 1
+
+    print("case 23 - first name with typo  and nickname: ",
+          name_processor.count_unique_names("Debor##ah", "Egli", "Deborah %", "Eg1li", "2Egli  Deb$bie"))# 1
 
 
 if __name__ == "__main__":
--- src/name_normalizer.py
+++ src/name_normalizer.py
@@ -1,8 +1,11 @@
+import re
+
 class NameNormalizer:
 
     def normalize(self, name):
-        name = name.lower().strip()
-        return self.remove_one_letter_name(name)
+        name1 = name.lower().strip()
+        clean_name = self.remove_non_alphabetical_except_spaces(name1)
+        return self.remove_one_letter_name(clean_name)
 
     def remove_one_letter_name(self, name):
         names = name.split()
@@ -13,3 +16,8 @@ class NameNormalizer:
         if len(names[1]) == 1:
             return names[0]
         return name
+
+    def remove_non_alphabetical_except_spaces(self, input_string):
+        # Use regex to replace all non-alphabetical characters except spaces with an empty string
+        cleaned_string = re.sub(r'[^a-zA-Z\s]', '', input_string)
+        return cleaned_string
